71e436927cefa39f99c14fe803704f45054af82a
Leon Silva <ls@openhealthtechnologies.com>
2025-10-08 13:34:47 -0300

    feat: remove old projects

--- dia1/agente_sem_ia/run.sh
+++
@@ -1,6 +0,0 @@
-cd finance_agent_local
-python -m venv .venv && . .venv/bin/activate
-pip install -r requirements.txt
-
-# Prepare seus CSVs em: finance_agent_local/data/acoes/PETR4.csv etc.
-python main.py --base-dir data/acoes --tickers PETR4 VALE3 --out output/relatorio.pdf --dt-ini 2024-01-01 --dt-fim 2024-12-31
